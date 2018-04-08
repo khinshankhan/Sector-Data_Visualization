c2ab08292804026711b003460472d28c96e51948
Khinshan Khan <kkhan@stuy.edu>
2018-04-08 15:15:39 -0400

    cleaned it up, will import things when needed, until tgn just minimal

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-import os
-from flask import Flask, render_template, session, redirect, url_for, request, flash
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 
 @app.route('/')
-def route_root():
+def root():
     return render_template('home.html')
 
 @app.route('/about')
-def route_about():
+def about():
     return render_template('about.html')
 
 @app.route('/visualization')
-def route_visualization():
+def visualization():
     return render_template('visualization.html')
 
 if __name__ == "__main__":
