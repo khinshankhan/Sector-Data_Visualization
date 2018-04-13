606e04614cd718de8a5b5e57323b889063b0614d
Ryan Siu <rsiu@stuy.edu>
2018-04-12 21:16:26 -0400

    Integrate table with correct answer animation

--- static/js/game.js
+++ static/js/game.js
@@ -34,12 +34,26 @@ var populateChart = function(data) {
     .attr("id", function(d) { return "text-" + d.data.answer.replace(" ", "-"); });
 
   var guess = document.getElementById("guess");
+  var table = document.getElementById("answer_table");
+
   guess.addEventListener("input", function() {
     for (var d in data) {
       if (guess.value == data[d].answer) {
-        var text = d3.select("#text-" + guess.value.replace(" ", "-"));
-        text.text(guess.value);
+
+        // handle pie chart
+        var pie_text = d3.select("#text-" + guess.value.replace(" ", "-"));
+        pie_text.text(guess.value);
+
+        // handle table
+        var rank = parseInt(d) + 1;
+        var tr = document.getElementById("tr_rank_" + rank);
+        var td = tr.getElementsByClassName("right")[0];
+        td.innerHTML = guess.value;
+
+        // handle input box
         guess.value = "";
+
+        // handle dataset to prevent repeats with the similar names
         data[d].answer = "";
       }
     }
