86e353eddce70a5cda8bd233dec4de726f40d5eb
Jake Zaia <jzaia@stuy.edu>
2018-04-18 22:07:25 -0400

    More temp

--- static/js/game.js
+++ static/js/game.js
@@ -197,14 +197,7 @@ var bargraph = function(d) {
     .call(d3.axisBottom(x));
 
   g.append("g")
-    .attr("class", "axis axis--y")
-    .call(d3.axisLeft(y).ticks(10, "%"))
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", "0.71em")
-    .attr("text-anchor", "end")
-    .text("Frequency");
+    .attr("class", "axis axis--y");
 
   g.selectAll(".bar")
     .data(data)
